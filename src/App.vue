8a9abbe9d231d6cd1f5bb695dfece7f8933fc936
Farris09 <69205257+Farris09@users.noreply.github.com>
2020-09-11 17:17:19 +0800

    oke

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,6 @@ export default {
  components: {
      SearchBar,
      VideoList,
-     // eslint-disable-next-line no-undef
      VideoDetail
  },
  data() {
--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
     <div v-if="video">
+        <div>
+            <iframe :src="videoUrl" />
+        </div>
         <div class="details">
             <h4>{{ video.snippet.title}}</h4>
             <p>{{ video.snippet.description }}</p>
@@ -10,6 +13,21 @@
 <script>
 export default {
     name:'VideoDetail',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        videoUrl() {
+            const {videoId} = this.video.id.videoId;
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+    }
 }
 </script>
+
+<style scoped>
+.details {
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid black;
+    border-radius: 4px;
+}
+</style>
